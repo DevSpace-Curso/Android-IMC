5f475c5e8bb957bd26be2bf5b92eb0169b753593
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2023-02-02 09:54:45 -0400

    atualização do projeto App IMC

--- app/src/main/java/com/example/meuprimeiroapp/MainActivity.kt
+++ app/src/main/java/com/example/meuprimeiroapp/MainActivity.kt
@@ -3,23 +3,28 @@ package com.example.meuprimeiroapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
         val btnCalcular: Button = findViewById(R.id.btnCalcular)
+        val textoMsg: EditText = findViewById(R.id.edtMsg)
+        val txtMessage: TextView = findViewById(R.id.txtMsg)
 
         btnCalcular.setOnClickListener{
-            val altura: Float = 1.75f * 1.75f
+            val message: String = textoMsg.text.toString()
+            txtMessage.text = message
+
+            println(message)
+
+           /* val altura: Float = 1.75f * 1.75f
             val peso: Int = 83
-
-            val result = peso / altura
-
-            println("Yasmim Ferreira " + result)
+            val result = peso / altura*/
+            //println("Yasmim Ferreira " + result)
         }
     }
 }
