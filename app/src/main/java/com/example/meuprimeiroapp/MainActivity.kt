98be8d255a1c16c6ba246b1fccfda1726cf308d4
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2023-02-09 11:20:25 -0400

    atualização do projeto IMC

--- app/src/main/java/com/example/meuprimeiroapp/MainActivity.kt
+++ app/src/main/java/com/example/meuprimeiroapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.meuprimeiroapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -14,15 +15,22 @@ class MainActivity : AppCompatActivity() {
         val btnCalcular: Button = findViewById(R.id.btnCalcular)
         val edtPeso: EditText = findViewById(R.id.edittext_peso)
         val edtAltura: EditText = findViewById(R.id.edittext_altura)
-        val tvResultado: TextView = findViewById(R.id.txtMsg)
+        /*val tvResultado: TextView = findViewById(R.id.txtMsg)*/
 
         btnCalcular.setOnClickListener{
-            val altura: Float = edtAltura.text.toString().toFloat()
+           val altura: Float = edtAltura.text.toString().toFloat()
             val alturaFinal: Float = altura * altura
             val peso: Float = edtPeso.text.toString().toFloat()
-            val result = peso / alturaFinal
+            val result: Float = peso / alturaFinal
 
-            tvResultado.text = result.toString()
+           /* tvResultado.text = result.toString()*/
+
+            val intent = Intent(this, ResultActivity::class.java)
+                .apply{
+                    putExtra("EXTRA_RESULT", result)
+                }
+
+            startActivity(intent)
         }
 
     }
--- app/src/main/java/com/example/meuprimeiroapp/ResultActivity.kt
+++ app/src/main/java/com/example/meuprimeiroapp/ResultActivity.kt
@@ -2,10 +2,44 @@ package com.example.meuprimeiroapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+
+        val tvResult = findViewById<TextView>(R.id.textview_result)
+        val tvClassificacao = findViewById<TextView>(R.id.textview_classificacao)
+        val result =  intent.getFloatExtra("EXTRA_RESULT", 0.1f)
+
+        tvResult.text = result.toString()
+
+
+        /* TABELA IMC
+        menor que 18,5      Abaixo do peso
+        entre 18,5 e 24,9   Normal
+        entre 25,0 e 29,9   Sobrepeso
+        entre 30,0 e 39,9   Obesidade
+        maior qUe 40,0      Obesidade Grave III
+         */
+
+
+        var classificacao = ""
+
+        if(result < 18.5f) {
+            classificacao = "ABAIXO DO PESO"
+        } else if (result >= 18.5 && result <= 24.9) {
+            classificacao = "NORMAL"
+        } else if(result >= 25.0 && result <= 29.9) {
+            classificacao = "SOBREPESO"
+        } else if(result >= 30.0 && result <= 39.9) {
+            classificacao = "OBESIDADE"
+        }  else {
+            classificacao = "OBESIDADE GRAVE III"
+        }
+
+       tvClassificacao.text = classificacao
     }
 }
